fb62ff8b7b013bc2715ac840b2b8041f5a2b936b
Tim Dams <dams.tim@telenet.Be>
2022-01-27 16:07:22 +0100

    Klaar?

--- Studentenklasse/Program.cs
+++ Studentenklasse/Program.cs
@@ -6,6 +6,8 @@ namespace Studentenklasse
     {
         static void Main(string[] args)
         {
+            //Kies eerst voor Build-> Rebuild solution, en wacht tot alle nodige packages werden gedownload.
+
             //Gebruik de bestaande Student.cs file om daarin je Studentklasse volgens de opgave te maken:
             //https://apwt.gitbook.io/ziescherp-oefeningen/zie-scherper-oefeningen-semester-2/h1-object-oriented-programming/a_practica#studentklasse
 
